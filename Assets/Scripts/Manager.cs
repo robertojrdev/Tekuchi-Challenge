0365e36ae988005eba46a6894471304b11171d03
Roberto Jr Dev <junior.rmg.rj@gmail.com>
2020-02-18 16:02:37 +0000

    simple loader done

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -3,6 +3,7 @@
 
 public class Manager : MonoBehaviour
 {
+    public Material objectsMaterial;
     public TextAsset jsonObjects;
     public TextAsset jsonColors;
 
@@ -11,10 +12,40 @@ public class Manager : MonoBehaviour
 
     private void Start()
     {
+        LoadFiles();
+        GenerateObject("mario");
+    }
+
+    private void LoadFiles()
+    {
         colors = JsonUtility.FromJson<ColorList>(
             jsonColors.ToString());
 
         combinations = JsonUtility.FromJson<CombinationList>(
             jsonObjects.ToString());
+    }
+
+    private void GenerateObject(string id)
+    {
+        Combination combination;
+
+        if(combinations.GetCombination(id, out combination))
+        {
+            foreach (var item in combination.items)
+            {
+                var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+                cube.transform.localPosition = item.transformData.position;
+                cube.transform.localScale = item.transformData.scale;
+                cube.transform.eulerAngles = item.transformData.rotation;
+                
+                Color color;
+                colors.GetColor(item.colorID, out color);
+
+                var renderer = cube.GetComponent<MeshRenderer>();
+                renderer.sharedMaterial = objectsMaterial;
+                renderer.material.SetColor("_Color", color);
+            }
+        }
     }
 }
